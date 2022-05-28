ac4eab6e1e5572f87b65b045a09abb9de8bb7067
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-05-28 16:46:37 +0430

    appendfilesync

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'This file was created by node js')
+fs.writeFileSync('notes.txt', 'my name is mohammadali')
+fs.appendFileSync('notes.txt', ' this text is added')
+

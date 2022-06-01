ed7f63d4564da2b36dd6ed4421da7bf04f39350a
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-06-01 20:19:12 +0430

    callback abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,5 @@
 const request = require('request');
-
+const geocode = require('./utils/geocode')
 // const url = "https://api.darksky.net/forecast/9bca3813cf3b181726b5d28283ee197b/35.655270, 51.387757?units=si"
 // request({ url: url , json: true }, (error, response) => {
 //       if (error) {
@@ -13,19 +13,24 @@ const request = require('request');
 //       }
 // })
 
-// geocoding
-// adress -> lat-lang  -> weather
 
-const url2 = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibW9oYW1tYWRhbGlmYWxsYWh6YWQiLCJhIjoiY2s1MjlzeWVlMDJvbzNqczBkYzg3YW9nNSJ9.YcbFFDaj5BKooq7hhLm2Vw"
-request({ url: url2, json: true }, (error, response) => {
-        if (error) {
-            console.log('Unable to connect to weather service!')
-      } else if (response.body.features.length === 0) {
-            console.log('Unable to find location')
-      } else {
-      const lat = response.body.features[0].center[0]
-      const long = response.body.features[0].center[1]
 
-      console.log(lat , long)
-      }
+// const url2 = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibW9oYW1tYWRhbGlmYWxsYWh6YWQiLCJhIjoiY2s1MjlzeWVlMDJvbzNqczBkYzg3YW9nNSJ9.YcbFFDaj5BKooq7hhLm2Vw"
+// request({ url: url2, json: true }, (error, response) => {
+//         if (error) {
+//             console.log('Unable to connect to weather service!')
+//       } else if (response.body.features.length === 0) {
+//             console.log('Unable to find location')
+//       } else {
+//       const lat = response.body.features[0].center[0]
+//       const long = response.body.features[0].center[1]
+//       console.log(lat , long)
+//       }
+// })
+
+
+
+geocode('boston', (error, data) => {
+ console.log('Error', error)
+ console.log('Data', data)
 })
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+
+
+const geocode = (address, callback) => {
+      const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibW9oYW1tYWRhbGlmYWxsYWh6YWQiLCJhIjoiY2s1MjlzeWVlMDJvbzNqczBkYzg3YW9nNSJ9.YcbFFDaj5BKooq7hhLm2Vw&limit=1'
+
+      request({ url: url, json: true }, (error, response) => {
+            if (error) {
+                  callback('Unable to connect to weather service!')
+            } else if (response.body.features.lenght === 0) {
+                  callback('Unable to find location. Try another search', undefined)
+            } else {
+                  callback(undefined, {
+                        latitude: response.body.features[0].center[0],
+                        longtitude: response.body.features[0].center[1],
+                        location: response.body.features[0].place_name
+                  })
+            }
+      })
+}
+
+
+module.exports = geocode

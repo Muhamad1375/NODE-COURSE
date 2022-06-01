f05dc0e5b9a7fa885f0cce8d866d4a0eba20cab1
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-06-01 15:47:27 +0430

    customize http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,8 @@
 const request = require('request');
 
-const url = "https://api.darksky.net/forecast/9bca3813cf3b181726b5d28283ee197b/37.8267,-122.4233"
+const url = "https://api.darksky.net/forecast/9bca3813cf3b181726b5d28283ee197b/35.655270, 51.387757?units=si"
 
-request({ url: url }, (error, response) => {
-      const data = JSON.parse(response.body)
-      console.log(data.currently)
-
+request({ url: url, json: true }, (error, response) => {
+      console.log(response.body.daily.data[0].summary +' this is currently '+ response.body.currently.temperature + ' degrees in javadieh. ' + 'there is a ' + response.body.currently.precipProbability + '% chance of rain')
 })
+

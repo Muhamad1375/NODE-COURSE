d250729a85aa2005828a0bad206b10d2e5a76abf
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-06-07 22:04:56 +0430

    serving up static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,11 @@
+const path = require('path')
 const express = require('express')
+
 
 const app = express()
 
-app.get('', (req, res) => {
-      res.send('<h1>Weather</h1>')
-})
-
-
-app.get('/help', (req, res) => {
-      res.send([{
-            name: 'Andrew'
-      },
-{
-      name: 'sara'
-}])
-})
-
-app.get('/about', (req, res) => {
-      res.send('<h1>About</h1>')
-})
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
 
 app.get('/weather', (req, res) => {

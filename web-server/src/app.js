3cc29e9aa66e9504bf86e86d109b78f628b5ff66
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-06-07 21:10:47 +0430

    express

---
+++ web-server/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+
+app.get('', (req, res) => {
+      res.send('<h1>Weather</h1>')
+})
+
+
+app.get('/help', (req, res) => {
+      res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+      res.send('About page')
+})
+
+app.get('/weather', (req, res) => {
+      res.send('View weather page')
+})
+
+
+app.listen(3000 , () => {
+      console.log('Server is up on port 3000')
+})

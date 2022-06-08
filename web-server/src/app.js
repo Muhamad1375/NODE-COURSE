b8f2e15c8299f11ce807bd69b9d3e0cf41390f5b
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-06-08 22:29:00 +0430

    dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,32 @@
 const path = require('path')
 const express = require('express')
 
-
 const app = express()
-
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+      res.render('index', {
+            title: 'Weather App',
+            name: 'Ali'
+      })
+})
+
+app.get('/about', (req, res) => {
+      res.render('about', {
+            title: 'About me',
+            name: 'Ali'
+      })
+})
+
+app.get('/help', (req, res) => {
+      res.render('help', {
+            helpText: 'This is some helpful text.',
+            name: 'Ali'
+      })
+})
 
 
 app.get('/weather', (req, res) => {

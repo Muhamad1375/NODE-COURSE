2f71f36812c87c21b8973c599dc9454948df8d90
Ali Alizade Haqiqi <aliz@x-matik.com>
2022-07-03 23:08:51 +0430

    use axios instead of old fashion request
    Signed-off-by: Ali Alizade Haqiqi <aliz@x-matik.com>

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -4,7 +4,8 @@ const request = require('request');
 
 const geocode = (address, callback) => {
       const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibW9oYW1tYWRhbGlmYWxsYWh6YWQiLCJhIjoiY2s1MjlzeWVlMDJvbzNqczBkYzg3YW9nNSJ9.YcbFFDaj5BKooq7hhLm2Vw&limit=1'
-
+    // DON'T USE FUCKING CALLBACK
+    // axios
       request({ url, json: true }, (error, { body }) => {
             if (error) {
                   callback('Unable to connect to weather service!')
